65d463f8d25236f9e30e5407dc1c3dbbd711d78c
mif99ms <msidqi86@gmail.com>
2018-10-29 21:50:37 +0700

    Initial commit

---
+++ src/pbo3/pkg10117096/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO 3
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
---
+++ src/pbo3/pkg10117096/latihan63/gradiengarislurus/PBO310117096Latihan63GradienGarisLurus.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117096.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author 
+ * Nama         : Miftah Muhammad Sidqi
+ * NIM          : 10117096
+ * Kelas        : PBO 3
+ * Deskripsi    : Program ini berisi program untuk menghitung gradien
+ */
+public class PBO310117096Latihan63GradienGarisLurus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Koordinat k1 = new Koordinat(2, 10, 5, 7);
+        Koordinat k2 = new Koordinat(5, 1, 3, 12);
+        
+        System.out.println("Garis yang melalui titik (2,10) dan (5,7) "
+                + "memiliki gradien sebesar " + k1.hitungGradien());
+        System.out.println("Garis yang melalui titik (5,1) dan (3,12) "
+                + "memiliki gradien sebesar " + k2.hitungGradien());
+    }
+    
+}
